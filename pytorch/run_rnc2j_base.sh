eff44c72cb233410815578f609b7e6303cb1b283
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2019-02-17 18:09:17 +0300

    rm data, add pytorch rnc2j config

--- pytorch/run_rnc2j_base.sh
+++ pytorch/run_rnc2j_base.sh
@@ -4,26 +4,26 @@ if [[ $1 == 'train' ]]; then
     echo 'Run training...'
     python train.py \
         --cuda \
-        --data ../data/wikitext-103/ \
-        --dataset wt103 \
+        --data ../data/rnc2j/ \
+        --dataset generic_dataset \
         --adaptive \
         --n_layer 8 \
         --d_model 256 \
         --n_head 8 \
         --d_head 32 \
-        --d_inner 2100 \
+        --d_inner 2048 \
         --dropout 0.1 \
         --dropatt 0.0 \
         --optim adam \
         --lr 0.00025 \
         --warmup_step 0 \
         --max_step 200000 \
-        --tgt_len 150 \
-        --mem_len 150 \
-        --eval_tgt_len 150 \
-        --batch_size 60 \
+        --tgt_len 128 \
+        --mem_len 128 \
+        --eval_tgt_len 128 \
+        --batch_size 32 \
         --multi_gpu \
-        --gpu0_bsz -1 \
+        --gpu0_bsz 8 \
         ${@:2}
 elif [[ $1 == 'eval' ]]; then
     echo 'Run evaluation...'
