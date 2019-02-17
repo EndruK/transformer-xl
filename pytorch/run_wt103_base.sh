057518bfeb3c4a3789f43118f89a519ba0ea4059
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2019-02-17 17:14:27 +0300

    Adjust pytorch wt103 base to two GPUs (same as TF version)

--- pytorch/run_wt103_base.sh
+++ pytorch/run_wt103_base.sh
@@ -7,10 +7,10 @@ if [[ $1 == 'train' ]]; then
         --data ../data/wikitext-103/ \
         --dataset wt103 \
         --adaptive \
-        --n_layer 16 \
-        --d_model 410 \
-        --n_head 10 \
-        --d_head 41 \
+        --n_layer 8 \
+        --d_model 256 \
+        --n_head 8 \
+        --d_head 32 \
         --d_inner 2100 \
         --dropout 0.1 \
         --dropatt 0.0 \
@@ -23,7 +23,7 @@ if [[ $1 == 'train' ]]; then
         --eval_tgt_len 150 \
         --batch_size 60 \
         --multi_gpu \
-        --gpu0_bsz 4 \
+        --gpu0_bsz -1 \
         ${@:2}
 elif [[ $1 == 'eval' ]]; then
     echo 'Run evaluation...'
