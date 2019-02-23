6817cbe6940bcea754c68a4f595764e2eaace93a
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2019-02-23 12:52:43 +0100

    More inference code, but predictions don't make sense

--- pytorch/inference.py
+++ pytorch/inference.py
@@ -1,6 +1,6 @@
 from pathlib import Path
+from typing import List, Tuple
 
-import numpy as np
 import sentencepiece as spm
 import torch
 import torch.cuda
@@ -40,7 +40,7 @@ class ModelWrapper:
         sp_processor.Load(str(spm_path))
         return cls(model, vocab, sp_processor, device)
 
-    def predict(self, text: str):
+    def tokenize(self, text: str) -> List[str]:
         tokens = []
         lines = text.split('\n')
         for i, line in enumerate(lines):
@@ -49,6 +49,15 @@ class ModelWrapper:
             assert not self.vocab.add_double_eos
             if self.vocab.add_eos and i != len(lines) - 1:
                 tokens.append(self.vocab.EOS)
+        return tokens
+
+    def predict_log_probs(self, tokens: List[str]) -> torch.Tensor:
+        """ Return log probabilities TODO
+        Shape of returned tensor is len(tokens) x len(self.vocab),
+        where TODO
+        """
+        if not tokens:
+            raise ValueError('tokens must be non-empty')
         all_xs = self.vocab.convert_to_tensor(tokens)
         all_log_probs = []
         with torch.no_grad():
@@ -59,6 +68,41 @@ class ModelWrapper:
                 xs = xs.to(device=self.device)
                 target = None
                 log_probs, mems = self.model(xs.unsqueeze(0), target, *mems)
-                log_probs = log_probs.squeeze(0).data.cpu().numpy()
+                log_probs = log_probs.squeeze(0).data.cpu()
                 all_log_probs.append(log_probs)
-        return np.concatenate(all_log_probs)
+        return torch.cat(all_log_probs)
+
+    def next_top_k(
+            self, tokens: List[str], top_k: int = 40,
+            ) -> List[Tuple[str, float]]:
+        """ Return top k tokens and their log probabilities.
+        """
+        log_probs = self.predict_log_probs(tokens)[-1]
+        top_indices = torch.argsort(log_probs)[-top_k:]
+        top_log_probs = log_probs[top_indices]
+        return [(self.vocab.idx2sym[idx], log_prob.item())
+                for idx, log_prob in
+                reversed(list(zip(top_indices, top_log_probs)))]
+
+    def sample_next(self, tokens: List[str], top_k: int = 40) -> str:
+        """ Sample next token from multinomial distribution.
+        """
+        tokens = tokens #+ [self.vocab.idx2sym[0]]
+        log_probs = self.predict_log_probs(tokens)[-1]
+        top_indices = torch.argsort(log_probs)[-top_k:]
+        top_probs = log_probs[top_indices].double().exp()
+        sampled_idx = top_indices[torch.multinomial(top_probs, 1).item()].item()
+        return self.vocab.idx2sym[sampled_idx]
+
+    def sample_text_iter(self, text: str, top_k: int = 40):
+        """ An iterator yielding pieces of generated text, resulting text
+        can be obtained by joining all of them with an empty string.
+        """
+        # TODO for longer texts we want to use memory and don't feed all tokens
+        tokens = self.tokenize(text)
+        while True:
+            next_token = self.sample_next(tokens, top_k=top_k)
+            # print(tokens, next_token)
+            yield (self.sp_processor.DecodePieces([tokens[-1], next_token])
+                   [len(self.sp_processor.DecodePieces([tokens[-1]])):])
+            tokens.append(next_token)
