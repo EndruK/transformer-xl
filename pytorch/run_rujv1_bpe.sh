fb11489ca4c6000573d27d5eaca3a641057c0a6a
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2019-03-04 11:35:53 +0300

    Train a bigger model with batch chunks
    Bigger batch size unsurprisingly helps

--- pytorch/run_rujv1_bpe.sh
+++ pytorch/run_rujv1_bpe.sh
@@ -8,23 +8,26 @@ if [[ $1 == 'train' ]]; then
         --cuda \
         --data ../data/rujv1_bpe/ \
         --dataset generic_dataset \
-        --n_layer 12 \
-        --d_model 512 \
-        --n_head 8 \
+        --n_layer 16 \
+        --d_model 768 \
+        --n_head 12 \
         --d_head 64 \
         --d_inner 2048 \
         --dropout 0.1 \
         --dropatt 0.0 \
         --optim adam \
         --lr 0.00025 \
-        --warmup_step 0 \
-        --max_step 480000 \
+        --warmup_step 2000 \
+        --max_step 100000 \
         --tgt_len 128 \
         --mem_len 128 \
         --eval_tgt_len 128 \
-        --batch_size 32 \
+        --batch_size 256 \
+        --batch_chunk 8 \
+        --eval-interval 1000 \
+        --log-interval 100 \
         --multi_gpu \
-        --gpu0_bsz -1 \
+        --gpu0_bsz 112 \
         ${@:2}
 else
     echo 'unknown argument 1'
